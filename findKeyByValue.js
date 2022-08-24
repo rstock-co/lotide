fc1159d7098d3a350de5bd332f7efe595b729691
Richard Stock <rstock.co@gmail.com>
2022-08-24 23:26:38 +0000

    Implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (bestShows, showTitle) => {
+  for (const [genre, show] of Object.entries(bestShows)) {
+    if (showTitle === show) return genre;
+  }
+}
+
+// TESTS
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  action: "John Wick",
+  reality: "Summer House",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "John Wick"), "action");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Summer House"), "reality");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
