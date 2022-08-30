bb15f502b7540bef732bf5073869aee56172c113
Richard Stock <rstock.co@gmail.com>
2022-08-30 03:10:14 +0000

    refactor eqArrays tests with M&C

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,24 +1,31 @@
-const assertEqual = require('../assertEqual');
-const eqArraysCore = require('../eqArrays').eqArraysCore;
-const eqArraysStretch = require('../eqArrays').eqArraysStretch;
+const assert = require("chai").assert;
+const eqArraysCore = require("../eqArrays").eqArraysCore;
+const eqArraysStretch = require("../eqArrays").eqArraysStretch;
 
-// --- CORE ---
-console.log('---CORE---');
+describe("#eqArrays - Core", () => {
+  it("should handle simple non-nested arrays", () => {
+    assert.strictEqual(eqArraysCore([1, 2, 3], [1, 2, 3]), true);
+    assert.strictEqual(eqArraysCore([1, 2, 3], [3, 2, 1]), false);
+    assert.strictEqual(eqArraysCore(["1", "2", "3"], ["1", "2", "3"]), true);
+    assert.strictEqual(eqArraysCore(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+});
 
-assertEqual(eqArraysCore([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArraysCore([1, 2, 3], [3, 2, 1]), false); 
-assertEqual(eqArraysCore(["1", "2", "3"], ["1", "2", "3"]), true); 
-assertEqual(eqArraysCore(["1", "2", "3"], ["1", "2", 3]), false); 
-
-// --- STRETCH ---
-console.log('---STRETCH---');
-
-// Nested Arrays (using recursion)
-const arr1 = [[2, 3], [4]];
-const arr2 = [[2, 3], [4, 5]];
-const arr3 = [[2, 3], 4];
-const arr4 = [[2, [3]], [4, [5, [6, [7, [8]], 9, [10]], [11]], [12,13]]];
-assertEqual(eqArraysStretch(arr1, arr1),true); 
-assertEqual(eqArraysStretch(arr1, arr2),false); 
-assertEqual(eqArraysStretch(arr1, arr3),false); 
-assertEqual(eqArraysStretch(arr4, arr4),true); 
+describe("#eqArrays - Stretch (using recursion)", () => {
+  it("should handle deeply nested arrays", () => {
+    const arr1 = [[2, 3], [4]];
+    const arr2 = [
+      [2, 3],
+      [4, 5],
+    ];
+    const arr3 = [[2, 3], 4];
+    const arr4 = [
+      [2, [3]],
+      [4, [5, [6, [7, [8]], 9, [10]], [11]], [12, 13]],
+    ];
+    assert.strictEqual(eqArraysStretch(arr1, arr1), true);
+    assert.strictEqual(eqArraysStretch(arr1, arr2), false);
+    assert.strictEqual(eqArraysStretch(arr1, arr3), false);
+    assert.strictEqual(eqArraysStretch(arr4, arr4), true);
+  });
+});
