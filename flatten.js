82ea0135cc8997063f756411e87923d852e5ea58
Richard Stock <rstock.co@gmail.com>
2022-08-30 02:24:04 +0000

    refactor flatten tests with M&C

--- flatten.js
+++ flatten.js
@@ -1,18 +1,3 @@
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) return false; // avoid having to loop if the arrays are not the same length
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: Array 1 === Array 2`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: Array 1 !== Array 2`);
-};
-
 const flatten = (array) => {
   let result = [];
   array.forEach((outerElement) => {
@@ -25,12 +10,4 @@ const flatten = (array) => {
   return result;
 };
 
-// TESTS
-
-const test1 = [1, 2, [3, 4], 5, [6]];
-const expected1 = [1, 2, 3, 4, 5, 6];
-assertArraysEqual(flatten(test1), expected1);
-
-const test2 = [['monkey', 'dog'],'giraffe',['tiger','lion','jaguar'],['rhino','hyena'],'wildebeast'];
-const expected2 = ['monkey', 'dog','giraffe','tiger','lion','jaguar','rhino','hyena','wildebeast'];
-assertArraysEqual(flatten(test2), expected2);
+module.exports = flatten;
---
+++ test/flattenTest.js
@@ -0,0 +1,18 @@
+const assert = require('chai').assert;
+const { italic } = require('ansi-colors');
+const flatten = require('../flatten');
+
+describe('#flatten', () => {
+  it('should return a flattened array, one level nested (numbers)', () => {
+    const array = [1, 2, [3, 4], 5, [6]];
+    const result = flatten(array);
+    const expected = [1, 2, 3, 4, 5, 6];
+    assert.deepEqual(result, expected);
+  });
+  it('should return a flattened array, one level nested (strings)', () => {
+    const array = [['monkey', 'dog'],'giraffe',['tiger','lion','jaguar'],['rhino','hyena'],'wildebeast'];
+    const result = flatten(array);
+    const expected = ['monkey', 'dog','giraffe','tiger','lion','jaguar','rhino','hyena','wildebeast'];
+    assert.deepEqual(result, expected);
+  });
+});
